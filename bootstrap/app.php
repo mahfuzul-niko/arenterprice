82eb28af1ebf3ce642e85118099a74f829315c83
Mahfuzul Islam <mahfuzul1125@gmail.com>
2024-11-02 19:12:46 +0600

    server

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::middleware(['web', 'auth'])->prefix('admin')->name('admin.')->group(base_path('routes/Admin.php'));
-            Route::middleware(['web', 'auth'])->prefix('agent')->name('agent.')->group(base_path('routes/Agent.php'));
+            Route::middleware(['web', 'auth'])->prefix('admin')->name('admin.')->group(base_path('routes/admin.php'));
+            Route::middleware(['web', 'auth'])->prefix('agent')->name('agent.')->group(base_path('routes/agent.php'));
         }
     )
     ->withMiddleware(function (Middleware $middleware) {
